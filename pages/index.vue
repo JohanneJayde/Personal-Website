f31fa0b89bb229cc92d72e8a21be7f48bfc8fa92
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-06-15 12:23:40 -0700

    adding in basic cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,48 @@
 <template>
   <v-container>
-    <v-card color="primary">
-      <v-card-title>Reliable, Reputable, and Relatable </v-card-title>
-      <v-card-text>
-        Hello, My name is Johanne McClenahan. I want to help build web
-        applications in the Spokane area to build up the community's economy.
-        I’m currently looking for a job in the web application development
-        field. I would prefer to have a job in front-end web development as I
-        have experience in HTML and CSS. However, I do have experience working
-        with MySQL and will learn PHP by April 2023, so I’m not opposed to
-        working in back-end development.
-      </v-card-text>
-    </v-card>
+    <v-col cols="12">
+      <v-card color="primary">
+        <v-card-title>About Me</v-card-title>
+        <v-card-text>
+          Hello, My name is Johanne McClenahan, and I recently moved to Spokane
+          three years ago. I currently attend Eastern Washington University and
+          expect to graduate in the Spring of 2024. My major is Computer Science
+          with a minor in Web Application Development. My hobbies include
+          puzzles, hiking, and scootering.
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
+      <v-sheet class="pa-1 text-center">
+        <v-card-title>Jobs</v-card-title>
+      </v-sheet>
+    </v-col>
+
+    <v-col cols="4">
+      <v-card>
+        <v-card-title>High School Tutor</v-card-title>
+        <v-card-subtitle
+          >(October 21st, 2018 to December 17th, 2018)</v-card-subtitle
+        >
+        <v-card-text>
+          Tutored Students who had trouble passing certain subjects for an hour
+          for two days a week.
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="4">
+      <v-card>
+        <v-card-title
+          >Engineering Intern, Electric (Utility) Department, City of
+          Sitka</v-card-title
+        >
+        <v-card-subtitle>(July 16th, 2019 to Sept. 17th, 2019)</v-card-subtitle>
+        <v-card-text>
+          Scanned, filed, and logged over 1425 Alaska Lumber and Pulp Company
+          drawings with a value above $1,000,000. Worked with AutoCad to edit
+          large-scale building schematics.
+        </v-card-text>
+      </v-card>
+    </v-col>
   </v-container>
 </template>
