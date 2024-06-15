7445f1006b07637a6f437a170bfaee4f012e1ec1
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-06-15 13:12:49 -0700

    update site title

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar flat>
       <v-app-bar-title class="text-center">
-        Johanne McClenahan
+        Johanne McClenahan Site
       </v-app-bar-title>
     </v-app-bar>
     <v-main>
